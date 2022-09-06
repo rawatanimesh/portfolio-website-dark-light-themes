45305e714bd1020a1758608c5b4f848ed13102c3
Animesh Rawat <animesh.rawat20@gmail.com>
2022-09-07 00:53:57 +0530

    added shell script to copy files in public folder

---
+++ copyfiles.sh
@@ -0,0 +1,12 @@
+pwd
+# cd /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes
+cd  /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/
+mkdir public
+cp /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/index.html /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/manifest.json /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/serviceWorker.js /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/404.html /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cd  /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/
+mkdir assets
+cp -r /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/assets/* /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/public/assets
+# cp ../index.html ../public/
